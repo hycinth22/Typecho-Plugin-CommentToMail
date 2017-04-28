b8bdaad993b7f1d17cb0133405a454bdf15fb259
rainfroid <aprikyblue@gmail.com>
2017-04-28 22:58:15 +0800

    sql file

--- CommentToMail/SQLite.sql
+++ CommentToMail/SQLite.sql
@@ -1,1 +1,6 @@
-CREATE TABLE IF NOT EXISTS `typecho_mail` (  `id` INTEGER NOT NULL PRIMARY KEY,  `content` text NOT NULL,  `sent` tinyint(1) default '0',  `log` text);
+CREATE TABLE IF NOT EXISTS `typecho_mail` (
+  `id` INTEGER NOT NULL PRIMARY KEY,
+  `content` text NOT NULL,
+  `sent` tinyint(1) default '0',
+  `log` text
+);
